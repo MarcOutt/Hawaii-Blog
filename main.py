4fd51b05a6757b1d6f40811619202cd4f53061fe
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2024-01-10 18:02:47 +0100

    add email functionality to contact form

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+import smtplib
+
+from flask import Flask, render_template, request
+
+from personnal_infos import my_email, password
 
 app = Flask(__name__)
 
@@ -13,8 +17,22 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        name = request.form.get("name")
+        phone = request.form.get("phone")
+        message_content = request.form.get("message")
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="your-email",
+                                msg=f"Name: {name}\nEmail: {email}\nPhone: {phone}\n\nMessage:\n{message_content}"
+                                )
+        return "Form submitted. Thank you for contacting me!"
     return render_template("contact.html")
 
 
